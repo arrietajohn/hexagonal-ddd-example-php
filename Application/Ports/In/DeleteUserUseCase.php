729522b84e01105670932a1e47af1d90409899b5
John Calor Arrieta Arrieta <johncalorarrietaarrieta@gmail.com>
2026-03-23 11:14:00 -0500

    feat(application): add input ports.  Use case interface contracts

---
+++ Application/Ports/In/DeleteUserUseCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../../Services/Dto/Commands/DeleteUserCommand.php';
+
+interface DeleteUserUseCase
+{
+    public function execute(DeleteUserCommand $command): void;
+}
+
---
+++ Application/Ports/In/LoginUseCase.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+interface LoginUseCase
+{
+    public function execute(LoginCommand $command): UserModel;
+}
