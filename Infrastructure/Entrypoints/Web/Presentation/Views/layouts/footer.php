048ea1f395ef50ac019e16f46de71e509101ed2d
John Calor Arrieta Arrieta <johncalorarrietaarrieta@gmail.com>
2026-03-25 16:59:26 -0500

    feat(layouts): add header, footer, and menu components for improved page structure

---
+++ Infrastructure/Entrypoints/Web/Presentation/Views/layouts/footer.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types=1); ?>
+<script src="../Infrastructure/Entrypoints/Web/Presentation/js/validaciones.js"></script>
+</body>
+</html>
---
+++ Infrastructure/Entrypoints/Web/Presentation/Views/layouts/header.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars($pageTitle ?? 'CRUD Usuarios', ENT_QUOTES, 'UTF-8') ?></title>
+    <link rel="stylesheet" href="../Infrastructure/Entrypoints/Web/Presentation/css/styles.css">
+</head>
+<body>
